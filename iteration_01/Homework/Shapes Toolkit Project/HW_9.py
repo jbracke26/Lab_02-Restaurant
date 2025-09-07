06a138a710bd6afa2aaee705a5ebf60e30d2e0bf
EbenLewis <elewis27@nmhschool.org>
2025-09-07 12:49:49 -0400

    commit shapes project

--- iteration_01/Homework/Shapes Toolkit Project/HW_9.py
+++ iteration_01/Homework/Shapes Toolkit Project/HW_9.py
@@ -13,17 +13,14 @@ class triangle:
     def __init__(self, tlength: int, twidth: int):
         self.height = twidth
         self.length = tlength
-        self.hyp = cmath.sqrt(((twidth * twidth) + (tlength * tlength)))
     def area(self) -> int :
         return 1/2 * (self.height * self.length)
-    def hypotenuse(self) -> float :
-        return self.hyp
     def discribe(self) -> None:
-        print(f"Triangle with length {self.length}, height {self.height}, and hypotenuse {self.hyp}")
+        print(f"Triangle with length {self.length}, height {self.height}")
 class circle: 
     def __init__(self, radius: int):
         self.radius = radius
     def area(self) -> float :
-        return cmath.pi * 2 * self.radius
+        return 3.1415926 * 2 * self.radius
     def discribe(self) -> None :
-        print(f"Circle with radius {self.radius} and area {circle.area()}")
+        print(f"Circle with radius {self.radius} and area {circle.area(self)}")
