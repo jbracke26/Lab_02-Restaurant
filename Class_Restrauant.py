e9d6d7bb89d9c51ed7a326039a9c285e3ad68d24
EbenLewis <elewis27@nmhschool.org>
2025-09-08 10:00:02 -0400

    initiate classes and start code

---
+++ Class_Restrauant.py
@@ -0,0 +1,12 @@
+class restaraunt: 
+    def __init__(self):
+        pass
+    def location(self):
+        location = "1 Lamplighter Way, Gill MA 01354"
+        print(location)
+    def cuisine(self):
+        cuisine = "USA BBQ"
+        print(cuisine)
+    def name(self):
+        name = "HBQ"
+        print(name)
