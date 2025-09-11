af8183f0d7bd891d216f7cf590924631156dd9b9
HenryBracke <jbracke26@nmhschool.org>
2025-09-11 10:11:38 -0400

    Added popup for add item and other gui functionality

--- gui.py
+++ gui.py
@@ -1,16 +1,44 @@
 from PySide6.QtWidgets import (
     QApplication,
     QMainWindow,
-    QLabel, 
+    QLabel,
     QPushButton,
     QVBoxLayout,
     QWidget,
+    QListWidget,
+    QHBoxLayout,
+    QDialog,
     QTextEdit,
-    QSlider,
-    QListWidget, 
-    QHBoxLayout, 
-    )
+    QComboBox,
+)
 from PySide6.QtCore import Qt
+
+
+class AddPopup(QDialog):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.setWindowTitle("Add Item")
+
+        self.layout = QVBoxLayout(self)
+
+        self.label = QLabel("Add Item")
+        self.label.setAlignment(Qt.AlignCenter)
+
+        self.Name = QTextEdit("Name")
+        self.Price = QTextEdit("Price")
+
+        self.Category = QComboBox()
+        self.Category.addItems(["Appetizers & Sides", "Entrees", "Desserts", "Drinks"])
+
+        self.Save = QPushButton("Save")
+
+        self.Save.clicked.connect(self.close)
+        self.layout.addWidget(self.label)
+        self.layout.addWidget(self.Name)
+        self.layout.addWidget(self.Price)
+        self.layout.addWidget(self.Category)
+        self.layout.addWidget(self.Save)
 
 
 class MainWindow(QMainWindow):
@@ -26,42 +54,34 @@ class MainWindow(QMainWindow):
 
         label = QLabel("Welcome to Generic Restaurant Software")
         label.setAlignment(Qt.AlignCenter)
-        
+
         inner_container = QWidget()
         inner_layout = QHBoxLayout(inner_container)
 
-        AddItem = QPushButton("Add Item")
+        self.AddItem = QPushButton("Add Item")
         SubItem = QPushButton("Remove Item")
-        SaveJSON = QPushButton("Save JSON")
 
-        #SubItem.clicked.connect(remove_item)
-   
-        inner_layout.addWidget(AddItem)
+        self.AddItem.clicked.connect(self.open_add_popup)
+        # SubItem.clicked.connect(remove_item)
+
+        inner_layout.addWidget(self.AddItem)
         inner_layout.addWidget(SubItem)
-        inner_layout.addWidget(SaveJSON)
 
         list_widget = QListWidget()
         list_widget.addItems(["Item 1", "Item 2", "Item 3"])
-
-
-
-
 
         layout.addWidget(inner_container)
         layout.addWidget(list_widget)
         layout.addWidget(label)
 
-        def remove_item(remove):
-            item = list_widget.currentItem()
-            #removeitem()
-            #refresh()
+    def remove_item(self, remove):
+        # TODO: Implement item removal functionality
+        pass
 
-            
-
-
-
-
-
+    def open_add_popup(self):
+        popup = AddPopup(self)
+        popup.exec()
+        pass
 
 
 app = QApplication()
