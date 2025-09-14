28aa59c2ab1c13fa58c31495ca6fca518872c932
EbenLewis <elewis27@nmhschool.org>
2025-09-14 19:12:05 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
 from Class_Restaurant import restaurant
 restaurant = restaurant()
-# FIXED: menuadd, menuchange, and data functions both work now
+# FIXED: menuadd, menuchange, menuremove, and data functions both work now
 restaurant.menuremove()
